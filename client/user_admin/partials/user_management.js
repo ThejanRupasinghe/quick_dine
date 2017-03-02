a2f54c032d0c736089bee5b725527197c2243185
ThejanRupasinghe <thejan94@gmail.com>
2017-03-02 20:10:31 +0530

    user edit ui completed

--- client/user_admin/partials/user_management.js
+++ client/user_admin/partials/user_management.js
@@ -46,8 +46,35 @@ Template.user_record.events({
         if(confirm("Are you sure want to delete this user ?")){
             Meteor.call('deleteUserFromAdmin',this._id);
         }
-    },
-    'click #edit-user':function(){
-        
+    }
+});
+
+Template.user_edit_form.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var name = $('[name=name]').val();
+        var username = $('[name=username]').val();
+        var password = $('[name=password]').val();
+        var repassword = $('[name=repassword]').val();
+        var role = $('[name=role]').val();
+
+        if (!(password===repassword)){
+            alert("Passwords don't match");
+        }else{
+            Meteor.call('createUserFromAdmin',username,password,role,name,function(error){
+                if(error !== undefined){
+                    alert(error.reason);
+                    $('[name=username]').val("");
+                    $('[name=password]').val("");
+                    $('[name=repassword]').val("");
+                }else{
+                    alert("User created successfully !");
+                    $('[name=name]').val("");
+                    $('[name=username]').val("");
+                    $('[name=password]').val("");
+                    $('[name=repassword]').val("");
+                }
+            });
+        }
     }
 });
