1da5b39f90dee9e5e1c2d96ab916da8273ba43cd
ThejanRupasinghe <thejan94@gmail.com>
2017-03-05 17:59:19 +0530

    minor changes

--- client/user_admin/menu_management/menu_management.js
+++ client/user_admin/menu_management/menu_management.js
@@ -1,12 +1,29 @@
-Template.menu_management.onCreated(function () {
+//ADD MENU ITEMS
+Template.add_menu_items.onCreated(function () {
     var self = this;
     self.autorun(function () {
         self.subscribe('categories');
     });
 });
 
-Template.menu_management.helpers({
+Template.add_menu_items.helpers({
     categories: ()=>{
         return Categories.find({});
     }
 });
+//----
+
+//EDIT MENU ITEMS
+Template.edit_menu_items.onCreated(function () {
+    var self = this;
+    self.autorun(function () {
+        self.subscribe('categories');
+    });
+});
+
+Template.edit_menu_items.helpers({
+    categories: ()=>{
+        return Categories.find({});
+    }
+});
+//----
