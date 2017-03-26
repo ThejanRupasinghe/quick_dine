3c1092dd7f8e7b4fee6f0fb8c3561d215855e91f
ThejanRupasinghe <thejan94@gmail.com>
2017-03-26 14:35:06 +0530

    adding menu items to  list done

--- client/user_waiter/waiter_index.js
+++ client/user_waiter/waiter_index.js
@@ -1,5 +1,6 @@
 //TEMP VARIABLE FOR NEW ORDER
-let new_order = {};
+let new_order = {menuItems: []};
+let clicked_item;
 
 //NEW ORDER
 Template.new_order.onCreated(function () {
@@ -8,7 +9,6 @@ Template.new_order.onCreated(function () {
     //adding tableNo to the new_order temp object
     route = Router.current();
     new_order.tableNo = route.params.tableNo;
-    console.log(new_order);
 
     self.autorun(function () {
         self.subscribe('categories');
@@ -25,9 +25,9 @@ Template.new_order.helpers({
 //CATEGORY BUTTON
 Template.category_button.events({
     'click #category_btn': function(){
-        var myContainer = document.getElementById('menuItemContainer');
-        myContainer.innerHTML = '';
-        Blaze.renderWithData(Template.menu_item_buttons,{id: Template.instance().data._id},myContainer);
+        var menuItemContainer = document.getElementById('menuItemContainer');
+        menuItemContainer.innerHTML = '';
+        Blaze.renderWithData(Template.menu_item_buttons,{id: Template.instance().data._id},menuItemContainer);
     }
 });
 //----
@@ -49,10 +49,21 @@ Template.menu_item_buttons.helpers({
 
 //MENU ITEM BUTTON
 Template.menu_item_button.events({
+    'click .numberButton': function(event){
+        $("#numberDisplay").append(event.target.textContent.trim());
+    },
+    'click #addMenuItem': function(){
+        let quantity = $("#numberDisplay").html();
+        new_order.menuItems.push({name: clicked_item, quantity: quantity});
+        $('#myModal').modal('hide');
+        var itemListContainer = document.getElementById('itemListContainer');
+        itemListContainer.innerHTML = '';
+        Blaze.renderWithData(Template.item_list,{new_order: new_order, tableNo: new_order.tableNo},itemListContainer);
+        console.log(new_order);
+    },
     'click #menu_item_btn': function(){
-        console.log(this._id);
-        console.log(this.name);
         $('#myModal').modal('show');
+        clicked_item = this.name;
     }
 });
 //----
