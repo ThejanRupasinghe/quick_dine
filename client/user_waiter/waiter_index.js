c0bfa0a296d7ea7b4afdea11b934e7ad3dc98b59
ThejanRupasinghe <thejan94@gmail.com>
2017-03-27 13:21:54 +0530

    removing added menu items and order schema

--- client/user_waiter/waiter_index.js
+++ client/user_waiter/waiter_index.js
@@ -2,6 +2,14 @@
 let new_order = {menuItems: []};
 let clicked_item;
 
+//WAITER HOME
+Template.waiter_home.events({
+    'click #newOrder': function () {
+        Router.go('waiter_new_order');
+    },
+
+});
+//----
 //NEW ORDER
 Template.new_order.onCreated(function () {
     var self = this;
@@ -57,16 +65,24 @@ Template.menu_item_button.events({
     },
     'click #addMenuItem': function(){
         let quantity = parseInt($("#numberDisplay").html().trim());
-        new_order.menuItems.push({name: clicked_item, quantity: quantity});
-        $('#myModal').modal('hide');
-        var itemListContainer = document.getElementById('itemListContainer');
-        itemListContainer.innerHTML = '';
-        Blaze.renderWithData(Template.item_list,{new_order: new_order, tableNo: new_order.tableNo, submit: true},itemListContainer);
-        console.log(new_order);
+        if(isNaN(quantity) || quantity==0){
+            $('#myModal').modal('hide');
+        }else{
+            new_order.menuItems.push({name: clicked_item, quantity: quantity});
+            $('#myModal').modal('hide');
+            var itemListContainer = document.getElementById('itemListContainer');
+            itemListContainer.innerHTML = '';
+            Blaze.renderWithData(Template.item_list,{new_order: new_order, tableNo: new_order.tableNo, submit: true},itemListContainer);
+            console.log(new_order);
+        }
     },
     'click #menu_item_btn': function(){
         $('#myModal').modal('show');
         clicked_item = this.name;
+    },
+    'click #cancelMenuItem':function () {
+        $('#numberDisplay').html('');
+        $('#myModal').modal('hide');
     }
 });
 //----
@@ -80,11 +96,25 @@ Template.item_list.events({
     'click #submit_order': function () {
         Meteor.call('addOrderFromWaiter',new_order.tableNo,new_order.menuItems,function (error) {
             if(error!==undefined) {
+                new_order = {menuItems: []};
                 $('#errors').html(error.reason);
             }else{
+                new_order = {menuItems: []};
                 Router.go('waiter_home');
             }
         });
+    },
+    'click .removeItem': function (event) {
+        var index = event.target.value;
+        new_order.menuItems.splice(index, 1);
+        var itemListContainer = document.getElementById('itemListContainer');
+        itemListContainer.innerHTML = '';
+        if(new_order.menuItems.length==0){
+            Blaze.renderWithData(Template.item_list,{new_order: new_order, tableNo: new_order.tableNo, submit: false},itemListContainer);
+        }else{
+            Blaze.renderWithData(Template.item_list,{new_order: new_order, tableNo: new_order.tableNo, submit: true},itemListContainer);
+        }
     }
 });
-//----
+
+//--
